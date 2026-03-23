4c73b5b32428afe9acd46a4303c6e7855131aa6a
Julia Gomes <julia_gomespereira@hotmail.com>
2026-03-23 17:24:18 -0300

    fixing light mode

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function toggleMode() {
 //se tiver com light mode, adicionar a image light 
 // se tiver sem light mode, manter a imagem normal 
 
-const img = document.querySelector("profile img")
+const img = document.querySelector("#profile img")
 if(html.classList.contains('light')) {
     img.setAttribute('src', './assets/avatar-light.png')
 
